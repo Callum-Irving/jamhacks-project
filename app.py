4ea43625621f15bc94b0b77cffa6ebb32bc06199
Callum Irving <callum.irving04@gmail.com>
2021-05-23 09:41:03 -0400

    Completed server

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from datetime import datetime
 from flask import Flask, request, jsonify
 import psycopg2
 
@@ -13,27 +14,66 @@ profanity_api_headers = {
     'x-rapidapi-host': "neutrinoapi-bad-word-filter.p.rapidapi.com"
 }
 
+conn = psycopg2.connect(
+    database=os.environ['DB_DATABASE'],
+    user=os.environ['DB_USER'],
+    password=os.environ['DB_PASSWD'],
+    sslmode='require',
+    port=26257,
+    host=os.environ['DB_HOST'],
+    options=os.environ['DB_OPTIONS']
+)
+
 
 @app.route('/api/createpost', methods=['POST'])
 def index():
     data = ""
     try:
-        data = request.get_json()
+        data = request.get_json()['content']
     except:
-        print("Error: request not in json format")
-        error_msg = {"error": "Request not in JSON format"}
+        error_msg = {"error": "Request not in correct JSON format"}
         return jsonify(error_msg), 400
 
     response = requests.request(
-        "POST", profanity_filter_url, data=data, headers=profanity_api_headers)
+        "POST", profanity_filter_url, data=f"content={data}", headers=profanity_api_headers)
 
     if response.json()['is-bad']:
         error_msg = {"error": "Profanity was detected in your post"}
         return jsonify(error_msg), 400
-    else:
-        success_msg = {"success": "Data added to databse"}
-        return jsonify(success_msg)
+
+    with conn.cursor() as cur:
+        current_date_string = datetime.now().date()
+
+        # Get latest ID
+        cur.execute(
+            "SELECT id FROM JAMHACKS.posts ORDER BY id DESC LIMIT 1")
+        old_id = cur.fetchall()[0][0]
+        conn.commit()
+        new_id = old_id + 1
+
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS JAMHACKS.posts (id INT PRIMARY KEY, content STRING, date DATE)"
+        )
+        cur.execute(
+            "UPSERT INTO JAMHACKS.posts (id, content, date) VALUES (%s, %s, %s)", (new_id, data, current_date_string))
+        conn.commit()
+    success_msg = {"success": "Data added to databse"}
+    return jsonify(success_msg)
+
+
+@app.route('/api/getposts', methods=['GET'])
+def get_posts():
+    posts = []
+    with conn.cursor() as cur:
+        cur.execute(
+            "SELECT * FROM JAMHACKS.posts ORDER BY id DESC")
+        posts = cur.fetchall()
+        conn.commit()
+
+    return jsonify({"posts": posts})
 
 
 if __name__ == "__main__":
     app.run()
+    print("Done")
+    conn.close()
